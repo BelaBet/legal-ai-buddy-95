8048254a5e3ada9ccb42fc31e1a49e9989d057f2
BelaBet <r.2019uk@gmail.com>
2026-08-31 19:34:03 -0300

    fix: activate PDF extraction bridge in chat

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { installPdfChatBridge } from "./lib/pdf-chat-bridge";
 
+installPdfChatBridge();
 createRoot(document.getElementById("root")!).render(<App />);
